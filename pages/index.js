bd91786fc3b2cc7d97bd0367f89aef7099da7e9d
Guilherme Andre de Melo <guilhermeanm95@hotmail.com>
2021-06-09 13:19:19 -0300

    create post blog

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,41 @@
-import Head from 'next/head'
-import CreatePost from '../components/CreatePost'
-
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import CreatePost from "../components/CreatePost";
+import fire from "../config/fire-config";
 
 const Home = () => {
-  return (
-   <div>
-     <Head>
-       <title>Blog App</title>
-     </Head>
-     <h1>Blog</h1>
-     <CreatePost />
-   </div>
-  )
-}
+  const [blogs, setBlogs] = useState([]);
 
-export default Home
+  useEffect(() => {
+    fire
+      .firestore()
+      .collection("blog")
+      .onSnapshot((snap) => {
+        const blogs = snap.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setBlogs(blogs);
+      });
+  }, []);
+
+  console.log(blogs)
+  return (
+    <div>
+      <Head>
+        <title>Blog App</title>
+      </Head>
+      <h1>Blog</h1>
+      <ul>
+        {blogs.map(blog =>
+          <li key={blog.id}>
+            {blog.title}
+          </li>
+        )}
+      </ul>
+      <CreatePost />
+    </div>
+  );
+};
+
+export default Home;
